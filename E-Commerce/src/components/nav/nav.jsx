4e39dffbfe81a9baf46ae9ec63f9e874628196f4
AbdullahShanto <abdurrahman7328@gmail.com>
2026-08-04 20:46:46 +0600

    nav section added

---
+++ E-Commerce/src/components/nav/nav.jsx
@@ -0,0 +1,46 @@
+import { FaShopify, FaShoppingCart } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+import "./nav.css";
+
+function Nav() {
+  return (
+    <header className="nav">
+      <div className="top-nav">
+        <div className="logo">
+          <div className="logo-icon">
+            <FaShopify />
+          </div>
+          <div>
+            <span className="brand-name">V-Shop</span>
+            <small>Modern essentials</small>
+          </div>
+        </div>
+
+        <form className="search-box" role="search">
+          <IoSearch />
+          <input type="text" placeholder="Search products..." />
+        </form>
+
+        <div className="nav-actions">
+          <a className="nav-link" href="#categories">
+            Categories
+          </a>
+          <div className="cart-box">
+            <FaShoppingCart />
+            <span>0</span>
+          </div>
+        </div>
+      </div>
+
+      <nav className="bottom-nav" aria-label="Secondary navigation">
+        <a href="#new">New In</a>
+        <a href="#deals">Deals</a>
+        <a href="#fashion">Fashion</a>
+        <a href="#tech">Tech</a>
+        <a href="#home">Home</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
---
+++ E-Commerce/src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import Nav from "../../components/nav/nav";
+
+function Home(){
+  return (
+    <div>
+     <Nav/>
+    </div>
+  )
+}
+
+export default Home;
